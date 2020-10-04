612c5c7e1b6038fc97c32f67e8c01096176adde8
Nubiluma <71071950+Nubiluma@users.noreply.github.com>
2020-10-04 16:32:02 +0200

    initial commit

---
+++ src/main/java/de/Nubiluma/Dijkstra/Dijkstra.java
@@ -0,0 +1,4 @@
+package de.Nubiluma.Dijkstra;
+
+public class Dijkstra {
+}
---
+++ src/main/java/de/Nubiluma/Dijkstra/Main.java
@@ -0,0 +1,9 @@
+package de.Nubiluma.Dijkstra;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
